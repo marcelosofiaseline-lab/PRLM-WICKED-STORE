d70509b1b6eaaaa1202d10b1b4cf1f236fb77e59
marcelo <marcelosofiaseline@gmail.com>
2025-11-29 21:54:30 +0800

    initial commit

---
+++ wickedDressShop.php
@@ -0,0 +1,138 @@
+<?php
+$shop = "The Wicked Shop!";
+
+$dresses = [
+    ["name" => "Glinda's Uniform", "price" => 799, "stock" => 10],
+    ["name" => "Glinda's Bubble Dress", "price" => 1499, "stock" => 3],
+    ["name" => "Elphie's Uniform", "price" => 599, "stock" => 8],
+    ["name" => "The Wicked Witch of the West's Dress", "price" => 999, "stock" => 4]
+];
+
+
+$discount = 10;
+$message = "All prices below already include a " . $discount . "% discount!";
+
+$discount1 = $dresses[0]["price"] * ($discount / 100);
+
+$final1 = $dresses[0]["price"] - $discount1;
+
+$discount2 = $dresses[1]["price"] * ($discount / 100);
+
+$final2 = $dresses[1]["price"] - $discount2;
+
+$discount3 = $dresses[2]["price"] * ($discount / 100);
+
+$final3 = $dresses[2]["price"] - $discount3;
+
+$discount4 = $dresses[3]["price"] * ($discount / 100);
+
+$final4 = $dresses[3]["price"] - $discount4;
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $shop; ?></title>
+    <style>
+        body {
+            background: linear-gradient(to right, lightgreen, #ffe4f2);
+            font-family: 'Wicked Font', sans-serif;
+            color: #7a0050;
+            padding: 20px;
+        }
+        .shopName {
+            font-family: 'Wicked Font Hollow', sans-serif;
+            text-align: center;
+            font-size: 60px;
+            background: linear-gradient(to right, black, green, hotpink, black);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            
+        }
+        table {
+            width: 80%;
+            margin: auto;
+            border-collapse: collapse;
+            background: white;
+            box-shadow: 0 0 10px #ff99cc;
+        }
+        th {
+            background: #ff66b2;
+            color: white;
+            padding: 12px;
+        }
+        tr {
+            background: linear-gradient(
+                to right,
+                rgba(144, 238, 144, 0.4),
+                rgba(255, 228, 242, 0.4)
+            );
+        }
+        td {
+            padding: 12px;
+            border-bottom: 1px solid #ffb3d9;
+            text-align: center;
+        }
+        .note {
+            text-align: center;
+            font-style: italic;
+            margin-bottom: 20px;
+        }
+
+        @font-face {
+            font-family: 'Wicked Font';
+            src: url('Wicked Font/WickedFont-Regular-BF65f34594abb81.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'Wicked Font Hollow';
+            src: url('Wicked Font/WickedFontHollow-Regular-BF65f3459671da4.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+    </style>
+</head>
+<body>
+
+<h1 class="shopName"><?php echo $shop; ?></h1>
+
+<p class="note"><?php echo $message; ?></p>
+
+<table>
+    <tr>
+        <th>Dress Name</th>
+        <th>Price (PHP)</th>
+        <th>Stock</th>
+    </tr>
+
+    <tr>
+        <td><?php echo $dresses[0]["name"]; ?></td>
+        <td><?php echo $final1; ?></td>
+        <td><?php echo $dresses[0]["stock"]; ?></td>
+    </tr>
+
+    <tr>
+        <td><?php echo $dresses[1]["name"]; ?></td>
+        <td><?php echo $final2; ?></td>
+        <td><?php echo $dresses[1]["stock"]; ?></td>
+    </tr>
+
+    <tr>
+        <td><?php echo $dresses[2]["name"]; ?></td>
+        <td><?php echo $final3; ?></td>
+        <td><?php echo $dresses[2]["stock"]; ?></td>
+    </tr>
+
+    <tr>
+        <td><?php echo $dresses[3]["name"]; ?></td>
+        <td><?php echo $final4; ?></td>
+        <td><?php echo $dresses[3]["stock"]; ?></td>
+    </tr>
+
+</table>
+
+</body>
+</html>
